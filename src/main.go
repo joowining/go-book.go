58921b981261a3b2a28b1e2cfad1077029d419c8
JoowonKim <99549623+joowining@users.noreply.github.com>
2024-06-09 10:42:38 +0900

    write go server
    write go server
    to send static index.html with CSS file
    with no-cache for applying modification in CSS file.

--- src/main.go
+++ src/main.go
@@ -1,7 +1,25 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+	"log"
+)
+
+const PORT = ":80"
 
 func main(){
-	fmt.Println("hello this is book.go")
+	fmt.Println("The server is running")
+	
+	http.HandleFunc("/", sendIndex )
+
+	log.Fatal(http.ListenAndServe(PORT, nil))
+}
+
+func sendIndex(w http.ResponseWriter, r *http.Request){
+	fs := http.FileServer(http.Dir("tmp"))
+	if r.URL.Path[len("/"):] == "style.css" {
+		w.Header().Set("Cache-Control", "no-cache, no-store, must-revalidate")
+	}
+	fs.ServeHTTP(w,r)
 }
